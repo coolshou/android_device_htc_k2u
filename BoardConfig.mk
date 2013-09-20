ef168459ca3a3a6713d4c9badf91d054c7b991be
Simon Sickle <simonsimons34@gmail.com>
2013-09-19 20:50:06 -0400

    We now compile with 4.3 source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31
 BOARD_KERNEL_BASE := 0x80400000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_FORCE_RAMDISK_ADDRESS := 0x81808000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x81808000
 TARGET_PREBUILT_KERNEL := device/htc/k2_u/prebuilt/kernel
 
 # fix this up by examining /proc/emmc on a running device
